9ef71e2671337b4048988f0b1b03693f396651bd
Kenny Johnston <kjohnston.ca@gmail.com>
2012-10-16 01:17:28 -0700

    Add comments to engine initializers.

--- lib/thincloud/postmark/engine.rb
+++ lib/thincloud/postmark/engine.rb
@@ -1,9 +1,12 @@
 class Engine < ::Rails::Engine
 
+  # Require the config initializer in advance so it is available for
+  # the "thincloud.postmark.action_mailer" initializer
   initializer "thincloud.postmark.configuration", before: "thincloud.postmark.action_mailer" do
     require File.expand_path("config/initializers/thincloud_postmark")
   end
 
+  # Apply the postmark settings just before ActionMailer applies them
   initializer "thincloud.postmark.action_mailer", before: "action_mailer.set_configs" do
     Postmark.secure = true
     Rails.application.config.action_mailer.delivery_method = :postmark
